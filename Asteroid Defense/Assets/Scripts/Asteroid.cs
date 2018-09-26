eba5be4aeaadb2fc8b7e365c6b3cfe43d9f8cbb4
Konstantinos Theodoridis <konstatheodoridis@gmail.com>
2018-09-26 17:19:09 +0300

    restriction when trying to create asteroids in same pos as another object as well as stationary asteroids

---
+++ Asteroid Defense/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+    void OnBecameInvisible() //Note: Takes into account every camera including the EDITOR view.. This shouldn't though cause any problems when this is built( even though it's weird in the dev process). For case of simplicity I used OnBecameInvisible
+    {
+        this.gameObject.SetActive(false);   
+    }
+}
--- Asteroid Defense/Assets/Scripts/GenerateAsteroids.cs
+++ Asteroid Defense/Assets/Scripts/GenerateAsteroids.cs
@@ -11,7 +11,8 @@ public class GenerateAsteroids : MonoBehaviour {
     public Vector3 spawnPos;
     public Vector3 mouseCurrLoc;
 
-    private bool locked = false;
+    private bool locked = false; // flag locked is used in order not to spawn multiple objects and holding down the mouse button
+    private bool valid = true;
     private float startTime;
     private float endTime;
     
@@ -27,34 +28,43 @@ public class GenerateAsteroids : MonoBehaviour {
             Instance = this;
     }
 
-    //flag locked is used in order not to spawn multiple objects and holding down the mouse button
     void Update () {
         if(Input.GetMouseButton(0) && !locked)
         {
             locked = true;
-            spawnPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            spawnPos.z = 0.0f; 
-            startTime = Time.time;
+            valid = AsteroidCanSpawn();
+            if (valid)
+            {
+                spawnPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                spawnPos.z = 0.0f;
+                startTime = Time.time;
+            }
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            float endTime = Time.time;
-            float duration = endTime - startTime;
+            if (valid)
+            {
+                float endTime = Time.time;
+                float duration = endTime - startTime; // the duration in seconds between the mouse button clicked and mouse button released(drag)
 
-            mouseCurrLoc = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 direction = mouseCurrLoc - spawnPos;
+                mouseCurrLoc = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector3 direction = mouseCurrLoc - spawnPos;
 
-            float distance = direction.magnitude;
-            float power = distance / duration;
+                float distance = direction.magnitude; // the distance of the drag
+                float power = distance / duration;    // calculate the power by taking into account the distance and duration of the drag
 
-            Vector3 m_Velocity = direction.normalized * power;
-            m_Velocity = new Vector3(m_Velocity.x, m_Velocity.y, 0.0f);
-            m_Velocity = Vector3.ClampMagnitude(m_Velocity, 10);
-            GameObject targetedAsteroid = CreateAsteroid();
-            
-            m_Rigidbody.velocity = m_Velocity; // simpl. w/o m_Velocity
-            Turret.Instance.ShootAsteroid(targetedAsteroid, spawnPos, m_Rigidbody.velocity);
+                Vector3 m_Velocity = direction.normalized * power;
+                m_Velocity = new Vector3(m_Velocity.x, m_Velocity.y, 0.0f);
+                m_Velocity = Vector3.ClampMagnitude(m_Velocity, 10);
+
+                GameObject targetedAsteroid = CreateAsteroid();
+
+                m_Rigidbody.velocity = m_Velocity; // simpl. w/o m_Velocity
+                if (m_Velocity == Vector3.zero)
+                    targetedAsteroid.SetActive(false);
+                Turret.Instance.ShootAsteroid(targetedAsteroid, spawnPos, m_Rigidbody.velocity);
+            }
             locked = false;
         }
 	}
@@ -66,5 +76,15 @@ public class GenerateAsteroids : MonoBehaviour {
         asteroidClone.SetActive(true);
         m_Rigidbody = asteroidClone.GetComponent<Rigidbody>();
         return asteroidClone;
+    }
+
+    //is false when we try to create an asteroid upon another object (e.g. safezone, another asteroid..) 
+    private bool AsteroidCanSpawn()
+    {
+        Vector3 ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, Vector3.forward, out hit))
+            return false;
+        return true;
     }
 }
